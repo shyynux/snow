e33fce4136414feea9692d76b3fd8b862b49771d
shyynux <shyynux@gmail.com>
2024-09-02 03:55:41 +0530

    remove unnecessary things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './Home';
-import Component from './components/Hero';
 import Create from './routes/Create';
 import Docs from './routes/Docs';
 import Pending from './routes/Pending';
@@ -11,7 +10,6 @@ const App = () => {
   return (
    <Routes>
     <Route path='/' element={<Home/>} />
-    <Route path='/sample' element={<Component/>} />
     <Route path='/create' element={<Create/>} />
     <Route path='/docs' element={<Docs/>} />
     <Route path='/uhoh' element={<Pending/>} />
